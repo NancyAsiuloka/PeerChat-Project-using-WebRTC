f107027a393c1b51cb54b0d6113920ed1fe73b70
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-02 12:48:50 +0100

    added js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({
         video: true, audio: false
     })
+    document.getElementById('user-1').srcObject = localStream;
 }
 
 init();
