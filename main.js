5a8d5259569958dcc76336f03614ba2cac46df31
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-06 14:42:24 +0100

    added a comment

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let localStream;
 let remoteStream;
 
 let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
     document.getElementById('user-1').srcObject = localStream;
 }
 
