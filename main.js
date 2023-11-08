87528c88489eb000bd193e8412baa0494f2a250d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-08 22:52:32 +0100

    added a conditional statement

--- main.js
+++ main.js
@@ -1,76 +1,97 @@
-let APP_ID = "8e6a8ba042c5479480b5d4cfe4a32b4e"
+let APP_ID = "8e6a8ba042c5479480b5d4cfe4a32b4e";
 
 let token = null;
-let uid = String(Math.floor(Math.random() * 10000))
+let uid = String(Math.floor(Math.random() * 10000));
 
 let client;
 let channel;
-
 
 let localStream;
 let remoteStream;
 let peerConnection;
 
 const servers = {
-    iceServers: [
-        {
-            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
-        }
-    ]
-}
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+};
 
 let init = async () => {
-    client = await AgoraRTM.createInstance(APP_ID)
-    await client.login({uid, token})
+  client = await AgoraRTM.createInstance(APP_ID);
+  await client.login({ uid, token });
 
-    // index.html?room=234234
-    channel = client.createChannel('main')
-    await channel.join()
+  // index.html?room=234234
+  channel = client.createChannel("main");
+  await channel.join();
 
-    channel.on('MemberJoined', handleUserJoined)
+  channel.on("MemberJoined", handleUserJoined);
 
-    client.on('MessageFromPeer', handleMessageFromPeer)
+  client.on("MessageFromPeer", handleMessageFromPeer);
 
-    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
-    document.getElementById('user-1').srcObject = localStream;
-
-}
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("user-1").srcObject = localStream;
+};
 
 let handleMessageFromPeer = async (message, MemberId) => {
-    console.log('Message:', message.text);
-}
+  message = JSON.parse(message.text);
+  console.log("Message:", message);
+};
 
 let handleUserJoined = async (MemberId) => {
-    console.log('A new user joined the channel:', MemberId);
-    createOffer(MemberId)
-}
+  console.log("A new user joined the channel:", MemberId);
+  createOffer(MemberId);
+};
 
 let createOffer = async (MemberId) => {
-    peerConnection = new RTCPeerConnection(servers);
+  peerConnection = new RTCPeerConnection(servers);
 
-    remoteStream = new MediaStream();
-    document.getElementById('user-2').srcObject = remoteStream;
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
 
-    localStream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track, localStream);
-    })
+  if (!localStream) {
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    document.getElementById("user-1").srcObject = localStream;
+  }
 
-    peerConnection.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-            remoteStream.addTrack(track);
-        })
-    };
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
 
-    peerConnection.onicecandidate = async (event) => {
-        if(event.candidate){
-            console.log('New ICE candidate:', event.candidate)
-        }
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      client.sendMessageToPeer(
+        {
+          text: JSON.stringify({
+            type: "candidate",
+            candidate: event.candidate,
+          }),
+        },
+        MemberId
+      );
     }
+  };
 
-    let offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
 
-    client.sendMessageToPeer({text: 'Hey!!!'}, MemberId)
-}
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "offer", offer: offer }) },
+    MemberId
+  );
+};
 
 init();
